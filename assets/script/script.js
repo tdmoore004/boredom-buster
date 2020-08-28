1cde58bc05b3189524780ecb68b16e78133a9354
Collin Porter <colls.potta@gmail.com>
2020-08-27 20:56:55 -0600

    added google books API

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,13 +4,27 @@ $(document).ready(function(){
 });
 
 
-// Bored-API call
-// let boredQueryUrl = "http://www.boredapi.com/api/activity/";
-
+//Bored-API call
 $('#searchBtn').on('click', function(event) {
     event.preventDefault();
     $.ajax({
         url: "http://www.boredapi.com/api/activity/",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    }).catch(function(error) {
+        console.log(error);
+    })
+
+})
+
+//Google Books API call
+var queryURL = "https://www.googleapis.com/books/v1/volumes?q=take a bubble bath" 
+
+$('#searchBtn').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+        url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(response);
