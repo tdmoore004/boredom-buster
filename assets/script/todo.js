23261c97662daa8890d597b1b8904641eedc8302
Tanner Moore <tdmoore004@gmail.com>
2020-09-02 17:41:56 -0600

    Appended activities to todo list.

--- assets/script/todo.js
+++ assets/script/todo.js
@@ -1,8 +1,11 @@
 // prepend localStorage
+let activityListDisplay = JSON.parse(localStorage.getItem('activityList'));
+for (let i = 0; i < activityListDisplay.length; i++) {
     let activityNameEl = $('<li>');
     activityNameEl.attr('class', 'collection-item');
-    activityName = localStorage.getItem('activityName');
-    activityNameEl.text(activityName);
-    $('#activityList').prepend(activityNameEl);
+    // activityName = localStorage.getItem('activityList');
+    activityNameEl.text(activityListDisplay[i]);
+    $('#activityList').append(activityNameEl);
+}
 
 
